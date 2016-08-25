01bfdf6e40753b567e846349c63862f979c43a92
qwazik <qwazik21@gmail.com>
2016-08-25 16:26:33 +0300

    update

--- js/main.js
+++ js/main.js
@@ -31,6 +31,34 @@
     }).animate({
         opacity: 1
     })
-  })
 
+    // popup callback
+    var popup = {
+        overlay: $('.popup__overlay'),
+        form: $('.popup__callback'),
+        close: $('.popup__close'),
+        init: function(){
+            this.listeners();
+        },
+        listeners: function(){
+            this.overlay.on('click', this.hide);
+            this.close.on('click', this.hide);
+        },
+        show: function(){
+            console.log('Открытие');
+            $('.popup__overlay').fadeIn(300);
+            $('.popup__callback').fadeIn(300);
+            return false;
+        },
+        hide: function(){
+            console.log('Закрытие');
+            $('.popup__overlay').fadeOut(300);
+            $('.popup__callback').fadeOut(300);
+            return false;
+        }
+    }
+    popup.init();
+
+    $('.callback-button').on('click', popup.show);
+  })
   
